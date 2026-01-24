21fe5c2284f069a6168fdf356ee20b4fc1094c60
youssefdh840 <tkyoyoussef@gmail.com>
2026-01-24 19:03:12 +0100

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,6 @@
+
+import { Fragrance, Outfit } from './types';
+
+// Hardcoded samples removed to ensure database-only synchronization.
+export const FRAGRANCES: Fragrance[] = [];
+export const OUTFITS: Outfit[] = [];
